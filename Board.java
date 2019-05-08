020ed4307bec333ad3dd2954ab59bd52f9af890f
huffmag <huffmag@wwu.edu>
2019-05-08 13:07:36 -0700

    Finished many many Classes.

--- Board.java
+++ Board.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,6 +8,7 @@ import java.io.FileNotFoundException;
 public class Board {
   private ArrayList<Scene> listOfScenes;
   private Location[] locations;
+  private Random r;
 
   public Board(int size) {
     locations = new Location[size];
@@ -21,8 +23,35 @@ public class Board {
     return count;
   }
 
-  public void endDay() {
+  //Note I changed this to take a list of players, just was useful
+  public void endDay(ArrayList<Player> players) {
+    for(int i = 0; i < locations.length; i++) {
+      int index = r.randInt(listOfScenes.size());
+      //Take them out Acting Locations
+      if(locations[i] instanceOf ActingLocation) {
+        locations[i].setScene(listOfScenes.get(index));
+        listOfScenes.remove(listOfScenes.get(index));
+        for(int j = 0; j < players.size(); j++) {
+          locations[i].removePlayer(players.get(j));
+        }
+      }
 
+      else {
+        //Take them out of Casting Office
+        if(locations[i] instanceOf CastingOffice) {
+          for(int j = 0; j < players.size(); j++) {
+            locations[i].removePlayer(players.get(j));
+          }
+        }
+        //Add them to Trailer
+        else{
+          for(int j = 0; j < players.size(); j++) {
+            locations[i].addPlayer(players.get(j));
+          }
+        }
+      }
+
+    }
   }
 
   public void printBoard() {
--- Role.java
+++ Role.java
@@ -13,10 +13,37 @@ public class Role {
     this.isMain = isMain;
   }
 
+  //Returns true if it was successful and false if it failed
+  public boolean take(Player player) {
+    if(takenBy == null) {
+      takenBy = player;
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  //May not ever be used
+  public void leave() {
+    takenBy = null;
+  }
+
   public void printRole() {
     System.out.println(name);
+    if(isMain) {
+      System.out.print("A Staring Role ");
+    }
+    if(takenBy != null) {
+      System.out.printf("taken by: %s", takenBy.getName());
+    }
+    System.out.println();
     System.out.println(line);
     System.out.println(rank);
     System.out.println();
+  }
+
+  public Player getPlayer(){
+    return takenBy;
   }
 }
