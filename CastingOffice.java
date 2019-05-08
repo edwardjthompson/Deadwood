fe6603b3cf5ff6c82a99768835a1d1ca6124df49
huffmag <huffmag@wwu.edu>
2019-05-08 08:25:50 -0700

    Added many print, and file reading functionalities.

--- CastingOffice.java
+++ CastingOffice.java
@@ -1,7 +1,12 @@
 
 public class CastingOffice extends Location {
+
+  public CastingOffice(String name) {
+    super(name);
+  }
+
   public void availableUpgrades(Player player) {
-    
+
   }
 
   public void selectUpgrade(Player player, int selectedRank) {
--- Deadwood.java
+++ Deadwood.java
@@ -1,5 +1,8 @@
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Deadwood {
   private int currentDay;
@@ -8,12 +11,21 @@ public class Deadwood {
   private int numPlayers;
   private ArrayList<Player> listOfPlayer;
 
-  public static void main(String[] args) {
+  public static final String PATHBOARD = "boardInfo.txt";
+  public static final String PATHSCENE = "sceneInfo.txt";
+  public static final int ROOMNUM = 12;
 
+  public static void main(String[] args) {
+    Deadwood deadwood = new Deadwood();
+    deadwood.initializeGame();
   }
 
   private void initializeGame() {
+    board = new Board(ROOMNUM);
+    createLocations();
+    createScenes();
 
+    board.printBoard();
   }
 
   private void startGame() {
@@ -25,7 +37,96 @@ public class Deadwood {
   }
 
   private void createLocations() {
+    File file;
+    Scanner s;
+    String line = "";
+    Location current = null;
 
+    try {
+      file = new File(PATHBOARD);
+      s = new Scanner(file);
+
+      while(s.hasNext()) {
+        line = s.nextLine();
+
+        if(line.equals("A-ROOM")) {
+          line = s.nextLine();
+          ActingLocation temp = new ActingLocation(line);
+          line = s.nextLine();
+          temp.setShots(Integer.parseInt(line));
+          current = temp;
+        }
+        else if(line.equals("T-ROOM")) {
+          line = s.nextLine();
+          current = new Location(line);
+        }
+        else if(line.equals("C-ROOM")) {
+          line = s.nextLine();
+          current = new CastingOffice(line);
+        }
+        else if(line.equals("ROLE")) {
+          String name = s.nextLine();
+          String quote = s.nextLine();
+          int rank = Integer.parseInt(s.nextLine());
+          Role role = new Role(rank, name, quote, false);
+          ActingLocation temp = (ActingLocation)current;
+          temp.addRole(role);
+        }
+        else {
+          board.addLocation(current);
+        }
+      }
+      s.close();
+    }
+    catch(FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    catch(NumberFormatException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void createScenes() {
+    File file;
+    Scanner s;
+    String line = "";
+    String title = "";
+    int br = 0;
+    Scene currentScene = null;
+    Role currentRole = null;
+
+    try {
+      file = new File(PATHSCENE);
+      s = new Scanner(file);
+
+      while(s.hasNext()) {
+        line = s.nextLine();
+
+        if(line.equals("SCENE")) {
+          title = s.nextLine();
+          line = s.nextLine();
+          br = Integer.parseInt(s.nextLine());
+          currentScene = new Scene(title, line, br);
+        }
+        else if(line.equals("ROLE")) {
+          title = s.nextLine();
+          line = s.nextLine();
+          br = Integer.parseInt(s.nextLine());
+          currentRole = new Role(br, title, line, true);
+          currentScene.addRole(currentRole);
+        }
+        else {
+          board.addScene(currentScene);
+        }
+      }
+      s.close();
+    }
+    catch(FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    catch(NumberFormatException e) {
+      e.printStackTrace();
+    }
   }
 
   private void createPlayers() {
